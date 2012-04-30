1ece091679ac01f7351a42057971c62f2e91e013
Kishore Mosaliganti <kishoreraom@gmail.com>
2012-04-30 11:26:23 -0400

    ENH: Megacapture.meg

--- PNGFlipper.sh
+++ PNGFlipper.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 #Set Path1 and Path2 variables correctly
-Path1="/home/krm15/data/Kishore/KishoreFengzhu/110911_forKishoreSomiteTimelapseGC/110911_session_2011_11_09__23_52_45/megacapture"
-Path2="/home/krm15/data/Kishore/KishoreFengzhu/110911_forKishoreSomiteTimelapseGC/110911_session_2011_11_09__23_52_45/newmegacapture"
+Path1="/home/krm15/data/Kishore/KishoreRamil/2011_12_28/nmegacapture"
+Path2="/home/krm15/data/Kishore/KishoreRamil/2011_12_28/nnmegacapture"
 
 
-for ch in {0,1}
+for ch in {0..0}
 do
-  for t in {0,1}
+  for t in {0..9}
   do
-    Files="${Path1}/*-TM0${t}*-ch0${ch}*.png"
+    Files="${Path1}/*-TM000${t}*-ch0${ch}*.png"
+    for a in ${Files}
+    do
+      filename=${a##*/}
+
+      cmd="convert -flip ${Path1}/${filename} ${Path2}/${filename}";
+
+      echo $cmd
+      eval $cmd
+    done
+  done
+done
+
+for ch in {0,0}
+do
+  for t in {10..52}
+  do
+    Files="${Path1}/*-TM00${t}*-ch0${ch}*.png"
     for a in ${Files}
     do
       filename=${a##*/}
